828e998674b53fb343cc5d3e223245882c56f58e
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-05-29 04:05:40 +0200

    Add ComponentArrays

--- src/Sleipnir.jl
+++ src/Sleipnir.jl
@@ -15,6 +15,7 @@ using Statistics
 using CairoMakie
 using Downloads
 using HDF5
+using ComponentArrays
 
 # ##############################################
 # ############    PARAMETERS     ###############
