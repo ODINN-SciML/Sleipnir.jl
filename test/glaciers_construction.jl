3f1e338ececf923f3d0fbc80b00d7278c280127b
Alban Gossard <alban.paul.gossard@gmail.com>
2025-01-25 14:57:33 +0100

    Fix the glathida processing part (#66)
    * fix the glathida processing part
    
    * bump version

--- test/glaciers_construction.jl
+++ test/glaciers_construction.jl
@@ -1,12 +1,20 @@
 
 
-function glaciers2D_constructor(; save_refs::Bool = false)
+function glaciers2D_constructor(; save_refs::Bool = false, use_glathida_data::Bool = false)
 
     rgi_paths = get_rgi_paths()
-    rgi_ids = ["RGI60-11.03638", "RGI60-11.01450"]
+    if use_glathida_data
+        rgi_ids = ["RGI60-07.00042", "RGI60-07.00065"] # Use glaciers that have glathida data
+        file_suffix = "w_glathida"
+    else
+        rgi_ids = ["RGI60-11.03638", "RGI60-11.01450"]
+        file_suffix = "wo_glathida"
+    end
+    # Filter out glaciers that are not used to avoid having references that depend on all the glaciers processed in Gungnir
+    rgi_paths = Dict(k => rgi_paths[k] for k in rgi_ids)
 
     params = Parameters(simulation=SimulationParameters(velocities=false,
-                                                        use_glathida_data=false,
+                                                        use_glathida_data=use_glathida_data,
                                                         working_dir=Sleipnir.root_dir,
                                                         test_mode=true,
                                                         rgi_paths=rgi_paths))
@@ -14,10 +22,10 @@ function glaciers2D_constructor(; save_refs::Bool = false)
     glaciers = initialize_glaciers(rgi_ids, params; test=true)
 
     if save_refs
-        jldsave(joinpath(Sleipnir.root_dir, "test/data/glaciers/glaciers2D.jld2"); glaciers)
+        jldsave(joinpath(Sleipnir.root_dir, string("test/data/glaciers/glaciers2D_", file_suffix, ".jld2")); glaciers)
     end
 
-    glaciers_ref = load(joinpath(Sleipnir.root_dir,"test/data/glaciers/glaciers2D.jld2"))["glaciers"]
+    glaciers_ref = load(joinpath(Sleipnir.root_dir, string("test/data/glaciers/glaciers2D_", file_suffix, ".jld2")))["glaciers"]
 
     @test all(glaciers == glaciers_ref)
 
