19889ea11d9142e96dd3a480608171a58f435081
Alban Gossard <alban.paul.gossard@gmail.com>
2025-02-24 14:19:01 +0100

    update jld2 files and bump version

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,11 +17,11 @@ include("plot_utils.jl")
 # Activate to avoid GKS backend Plot issues in the JupyterHub
 ENV["GKSwstype"]="nul"
 
-@testset "Run all tests" begin 
+@testset "Run all tests" begin
 
-@testset "Parameters constructors with specified values" params_constructor_specified()
+@testset "Parameters constructors with specified values" params_constructor_specified(save_refs=false)
 
-@testset "Parameters constructors by default" params_constructor_default()
+@testset "Parameters constructors by default" params_constructor_default(save_refs=false)
 
 @testset "Glaciers 2D constructors w/o glathida data" glaciers2D_constructor(use_glathida_data=false, save_refs=false)
 
