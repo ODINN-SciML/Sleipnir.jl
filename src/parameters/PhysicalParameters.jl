0b3536a706b7535772938a20427cdbb548685f8a
Facundo Sapienza <fsapienza@berkeley.edu>
2025-02-24 13:16:39 -0800

    Code for preprocessing Rabatel (2023) data (#71)
    * Calculatio of Transversal Mercator Projection for glacier coordinates
    
    * Fix typo and force run of all tests
    
    * Update test files
    
    * Add missng glaciers to see if fixes tests
    
    * Include zone projection in Mercator
    
    * [WIP] Adding methods to process Rabatel (2023) ice surface velocity data
    
    * Data adquisition of Rabatel data
    
    * Closes #3
    
    * Remove sync CI
    
    * restore original test data files
    
    * fix typo
    
    * Adding dates for interp dataset
    
    * Fix small comments

--- src/parameters/PhysicalParameters.jl
+++ src/parameters/PhysicalParameters.jl
@@ -13,28 +13,29 @@ end
 
 """
     PhysicalParameters(;
-        ρ::Float64 = 900.0,
-        g::Float64 = 9.81,
-        ϵ::Float64 = 1e-3,
-        η₀::F = 1.0, 
-        maxA::Float64 = 8e-17,
-        minA::Float64 = 8.5e-20,
-        maxTlaw::Float64 = 1.0,
-        minTlaw::Float64 = -25.0,
-        noise_A_magnitude::Float64 = 5e-18
-        )
+            ρ::F = 900.0,
+            g::F = 9.81,
+            ϵ::F = 1e-3,
+            η₀::F = 1.0, 
+            maxA::F = 8e-17,
+            minA::F = 8.5e-20,
+            maxTlaw::F = 1.0,
+            minTlaw::F = -25.0,
+            noise_A_magnitude::F = 5e-18
+            )
+
 Initialize the physical parameters of a model.
 Keyword arguments
 =================
     - `ρ`: Ice density
     - `g`: Gravitational constant
-    - `n`: Glen's exponent
-    - `A`: Glen's coefficient
     - `ϵ`: Small number
-    - `C`: Sliding coefficient
     - `η₀`:  
     - `maxA`: Maximum value for `A` (Glen's coefficient)
     - `minA`: Minimum value for `A` (Glen's coefficient)
+    - `maxTlaw`: Maximum value of Temperature used in simulations on fake law
+    - `minTlaw`: Minimum value of Temperature used in simulations on fake law
+    - `noise_A_magnitude`: Magnitude of noise added to A
 """
 function PhysicalParameters(;
             ρ::F = 900.0,
