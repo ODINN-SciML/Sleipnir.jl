9282d26c6a7dd8a62c41d43780f44dbed71f327d
Alban Gossard <alban.paul.gossard@gmail.com>
2025-03-29 16:41:57 +0100

    add dummy climate 2D + fix several bugs

--- src/glaciers/climate/Climate2D.jl
+++ src/glaciers/climate/Climate2D.jl
@@ -33,3 +33,45 @@ Base.:(==)(a::Climate2D, b::Climate2D) = a.raw_climate == b.raw_climate && a.cli
                                       a.climate_step == b.climate_step && a.climate_2D_step == b.climate_2D_step &&
                                       a.longterm_temps == b.longterm_temps && a.avg_temps == b.avg_temps &&
                                       a.avg_gradients == b.avg_gradients
+
+"""
+    DummyClimate2D(;
+        longterm_temps::Vector{F} = []
+    ) where {F <: AbstractFloat}
+
+Dummy climate initialization for very specific use cases where we don't have climate
+data and we need to build a minimalistic climate with only a few data.
+For the moment it supports only the initialization of the long term temperatures.
+It returns a minimalistic Climate2D instance.
+
+Arguments:
+- `longterm_temps::Vector{F}`: Long term temperatures.
+"""
+
+function DummyClimate2D(;
+    longterm_temps::Vector{F} = []
+) where {F <: AbstractFloat}
+    ras = Raster(rand(X(1:0), Y(1:0), Ti(DateTime(2001):Month(1):DateTime(2002))))
+    emptyRasterStack = RasterStack(ras)
+    dummyMatrix = [0.;;]
+    emptyClimate2Dstep = Climate2Dstep(
+        temp = dummyMatrix,
+        PDD = dummyMatrix,
+        snow = dummyMatrix,
+        rain = dummyMatrix,
+        gradient = 0.,
+        avg_gradient = 0.,
+        x = [0.],
+        y = [0.],
+        ref_hgt = 0.
+    )
+    return Climate2D(
+        raw_climate = emptyRasterStack,
+        climate_raw_step = emptyRasterStack,
+        climate_step = Dict(),
+        climate_2D_step = emptyClimate2Dstep,
+        longterm_temps = longterm_temps,
+        avg_temps = 0.,
+        avg_gradients = 0.,
+    )
+end
