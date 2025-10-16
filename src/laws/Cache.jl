cbbe37ec36cbb3f9975bc3902eb437561ca5590f
Alban Gossard <alban.paul.gossard@gmail.com>
2025-10-16 09:02:49 +0200

    rename float law by scalar law

--- src/laws/Cache.jl
+++ src/laws/Cache.jl
@@ -1,10 +1,11 @@
 import Base.similar, Base.size, Base.fill
-export MatrixCacheNoVJP, FloatCacheNoVJP, MatrixCache, FloatCache
+export MatrixCacheNoVJP, ScalarCacheNoVJP, MatrixCache, ScalarCache
 
 """
     MatrixCacheNoVJP
 
 A mutable cache structure for storing a two-dimensional array of `Float64` values.
+This is typically used for spatially varying laws.
 This struct is intended for use cases where the law is not differentiated, and hence the vector-Jacobian products (VJP) are not required.
 Fields:
 - `value::Array{Float64, 2}`: The cached matrix.
@@ -14,27 +15,29 @@ mutable struct MatrixCacheNoVJP
 end
 
 """
-    FloatCacheNoVJP
+    ScalarCacheNoVJP
 
 A mutable cache structure for storing a scalar value as a zero-dimensional array of `Float64`.
+This is typically used for constant per glacier laws.
 This struct is intended for use cases where the law is not differentiated, and hence the vector-Jacobian products (VJP) are not required.
 Fields:
 - `value::Array{Float64, 0}`: The cached scalar value.
 """
-mutable struct FloatCacheNoVJP
+mutable struct ScalarCacheNoVJP
     value::Array{Float64, 0}
 end
 
-similar(c::Union{FloatCacheNoVJP, MatrixCacheNoVJP}) = typeof(c)(similar(c.value))
-size(c::Union{FloatCacheNoVJP, MatrixCacheNoVJP}) = size(c.value)
-fill(c::Union{FloatCacheNoVJP, MatrixCacheNoVJP}, s) = typeof(c)(fill(c.value, s))
-Base.:(==)(a::Union{FloatCacheNoVJP, MatrixCacheNoVJP}, b::Union{FloatCacheNoVJP, MatrixCacheNoVJP}) = a.value == b.value
+similar(c::Union{ScalarCacheNoVJP, MatrixCacheNoVJP}) = typeof(c)(similar(c.value))
+size(c::Union{ScalarCacheNoVJP, MatrixCacheNoVJP}) = size(c.value)
+fill(c::Union{ScalarCacheNoVJP, MatrixCacheNoVJP}, s) = typeof(c)(fill(c.value, s))
+Base.:(==)(a::Union{ScalarCacheNoVJP, MatrixCacheNoVJP}, b::Union{ScalarCacheNoVJP, MatrixCacheNoVJP}) = a.value == b.value
 
 """
     MatrixCache
 
 A cache structure for storing a two-dimensional array of `Float64` values along with
 their associated vector-Jacobian products (VJP).
+This is typically used for spatially varying laws.
 Fields:
 - `value::Array{Float64, 2}`: The cached matrix.
 - `vjp_inp::Array{Float64, 2}`: VJP with respect to inputs.
@@ -47,22 +50,23 @@ struct MatrixCache
 end
 
 """
-    FloatCache
+    ScalarCache
 
 A cache structure for storing a scalar value as a zero-dimensional array of `Float64` along with
 their associated vector-Jacobian products (VJP).
+This is typically used for constant per glacier laws.
 Fields:
 - `value::Array{Float64, 0}`: The cached scalar value.
 - `vjp_inp::Array{Float64, 0}`: VJP with respect to inputs.
 - `vjp_θ::Vector{Float64}`: VJP with respect to parameters.
 """
-struct FloatCache
+struct ScalarCache
     value::Array{Float64, 0}
     vjp_inp::Array{Float64, 0}
     vjp_θ::Vector{Float64}
 end
 
-similar(c::Union{FloatCache, MatrixCache}) = typeof(c)(similar(c.value), similar(c.vjp_inp), similar(c.vjp_θ))
-size(c::Union{FloatCache, MatrixCache}) = size(c.value)
-fill(c::Union{FloatCache, MatrixCache}, s) = typeof(c)(fill(c.value, s), fill(c.vjp_inp, s), similar(c.vjp_θ))
-Base.:(==)(a::Union{FloatCache, MatrixCache}, b::Union{FloatCache, MatrixCache}) = a.value == b.value && a.vjp_inp == b.vjp_inp && a.vjp_θ == b.vjp_θ
+similar(c::Union{ScalarCache, MatrixCache}) = typeof(c)(similar(c.value), similar(c.vjp_inp), similar(c.vjp_θ))
+size(c::Union{ScalarCache, MatrixCache}) = size(c.value)
+fill(c::Union{ScalarCache, MatrixCache}, s) = typeof(c)(fill(c.value, s), fill(c.vjp_inp, s), similar(c.vjp_θ))
+Base.:(==)(a::Union{ScalarCache, MatrixCache}, b::Union{ScalarCache, MatrixCache}) = a.value == b.value && a.vjp_inp == b.vjp_inp && a.vjp_θ == b.vjp_θ
