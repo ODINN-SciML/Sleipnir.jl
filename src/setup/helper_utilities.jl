9787a09671c27612befd671fed5af5a290e8b6b2
Alban Gossard <alban.paul.gossard@gmail.com>
2026-05-15 16:16:26 +0200

    Fix: update safe_approx to handle Dict

--- src/setup/helper_utilities.jl
+++ src/setup/helper_utilities.jl
@@ -1,11 +1,14 @@
 export safe_approx
 
-# Function to override "≈" to handle nothing values
+# Function to override "≈" to handle nothing values and dict
 function safe_approx(a, b)
     if isnothing(a) && isnothing(b)
         return true
     elseif isnothing(a) || isnothing(b)
         return false
+    elseif (a isa Dict) && (b isa Dict)
+        keys(a) == keys(b) || return false
+        return all(a[k] ≈ b[k] for k in keys(a))
     else
         return a ≈ b
     end
